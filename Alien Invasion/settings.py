8b17aaaad52d1b58edd5ad14221ee13d8df0b1c3
van765 <s2pankratovich@gmail.com>
2025-07-11 17:55:07 +0300

    Add files via upload

---
+++ Alien Invasion/settings.py
@@ -0,0 +1,7 @@
+class Settings():
+	def __init__(self):
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (5, 30, 70)
+		self.ship_speed = 4
+		self.ship_vertical_speed = 1.5
