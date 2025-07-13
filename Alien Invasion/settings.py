41567ab1caa2fe286dc89048c8c1262f340ce2a5
van765 <s2pankratovich@gmail.com>
2025-07-14 00:04:47 +0300

    Add files via upload

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -1,19 +1,20 @@
 class Settings:
-    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
-        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (5, 30, 70)
 
-        # Ship settings.
-        self.ship_speed = 1.5
+        self.ship_speed = 4
+        self.ship_vertical_speed = 1.5
+        self.ship_limit = 3
 
-        # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullet_color = (255, 255, 255)
+        self.bullets_allowed = 10
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
